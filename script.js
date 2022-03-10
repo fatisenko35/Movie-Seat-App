f3eb96a6bf4c3da6ffcbd51259fd4455389d5489
fatisenko <fatihsenko@gmail.com>
2022-03-10 16:27:03 +0300

    local storage items added

--- script.js
+++ script.js
@@ -4,41 +4,51 @@ const count = document.getElementById("count");
 const film = document.getElementById("film");
 const total = document.getElementById("total")
 
-let value = movieSelect.options[movieSelect.selectedIndex].value;
-let text = movieSelect.options[movieSelect.selectedIndex].text;
+
+let arr = []
+
+movieSelect.addEventListener("change", () => {
+
+    film.innerText = movieSelect.options[movieSelect.selectedIndex].
+    text.slice(0, movieSelect.options[movieSelect.selectedIndex].text.length - 5);
+
+    total.innerText = count.innerText * movieSelect.options[movieSelect.selectedIndex].value;
+
+})
+
+seat.forEach((item) => {
 
 
-    movieSelect.addEventListener("change", () => {
-        film.innerText = movieSelect.options[movieSelect.selectedIndex].text.slice(0, movieSelect.options[movieSelect.selectedIndex].text.length - 5)
-        total.innerText = count.innerText * movieSelect.options[movieSelect.selectedIndex].value
+    item.addEventListener("click", (e) => {
+        film.innerText = movieSelect.options[movieSelect.selectedIndex].
+        text.slice(0, movieSelect.options[movieSelect.selectedIndex].text.length - 6);
+        
+        let temp = e.target.classList.value;
 
-    })
+        if (temp == "seat") {
+            e.target.classList.value = "seat selected"
+            count.innerText++;
+            arr.push(Array.from(seat).indexOf(e.target))
+            total.innerText = count.innerText * movieSelect.options[movieSelect.selectedIndex].value;
 
-    seat.forEach((item) => {
-
-
-        item.addEventListener("click", (e) => {
-            film.innerText = movieSelect.options[movieSelect.selectedIndex].text.slice(0, movieSelect.options[movieSelect.selectedIndex].text.length - 6)
-            let temp = e.target.classList.value;
-
-            if (temp == "seat") {
-                e.target.classList.value = "seat selected"
-                count.innerText++;
-                total.innerText = count.innerText * movieSelect.options[movieSelect.selectedIndex].value
-
-            } else {
-                if (temp == "seat occupied") {
-                    return;
-                }
-                else {
-                    e.target.classList.value = "seat"
-                    if (count.innerText > 0) count.innerText--;
-                    total.innerText = count.innerText * movieSelect.options[movieSelect.selectedIndex].value
-                }
+        } else {
+            if (temp == "seat occupied") {
+                return;
             }
-        })
+            else {
+                e.target.classList.value = "seat"
+                if (count.innerText > 0) count.innerText--;
+                total.innerText = count.innerText * movieSelect.options[movieSelect.selectedIndex].value;
+                arr.splice(arr.indexOf(Array.from(seat).indexOf(e.target)), 1);
 
+            }
+        }
+        localStorage.setItem("Seats", JSON.stringify(arr));
+        let array = localStorage.getItem("Seats");
+        array = JSON.parse;
     })
+
+})
 
 
 
